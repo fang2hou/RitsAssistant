96fae6d2cda4cd1c4307cad69d5150d16675265f
Zhou Fang <houshuu@live.jp>
2018-12-31 16:44:33 +0900

    Initial Commit

---
+++ RitsAssistant/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  RitsAssistant
+//
+//  Created by Zhou Fang on 2018/12/31.
+//  Copyright © 2018 Zhou Fang. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
