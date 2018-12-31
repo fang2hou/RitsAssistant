0a46abfd2a2f670e8551cdb2930cae99a477283a
Zhou Fang <houshuu@live.jp>
2018-12-31 16:54:25 +0900

    Cleanup auto-generated files.

---
+++ RitsAssistant/PopoverViewController.swift
@@ -0,0 +1,17 @@
+//
+//  PopoverViewController.swift
+//  RitsAssistant
+//
+//  Created by Zhou Fang on 2018/12/31.
+//  Copyright © 2018 Zhou Fang. All rights reserved.
+//
+
+import Cocoa
+
+class PopoverViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+}
